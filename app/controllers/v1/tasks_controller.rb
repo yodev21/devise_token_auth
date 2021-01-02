83dab6d96fa335cc3f0f29a857e817ab9ca1d328
yodev <yodev2177@gmail.com>
2021-01-02 18:34:18 +0900

    Task Create

--- app/controllers/v1/tasks_controller.rb
+++ app/controllers/v1/tasks_controller.rb
@@ -1,8 +1,20 @@
 class V1::TasksController < ApplicationController
+  
+  before_action :set_task, only: [:show]
+  before_action :authenticate_v1_user!
   def index
+    tasks = Task.all
+    render json: tasks
+  end
+
+  def create
+    user = User.find_by(email: params[:headers][:uid])
+    task = Task.new(title: params[:title], content: params[:content], user_id: user.id)
+    task.save
   end
 
   def show
+    render json: @task
   end
 
   def update
@@ -10,4 +22,10 @@ class V1::TasksController < ApplicationController
 
   def destroy
   end
+
+  private
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
   namespace :v1 do
-    get 'tasks/index'
-    get 'tasks/show'
-    get 'tasks/update'
-    get 'tasks/destroy'
-  end
-  namespace :v1 do
+    resources :tasks
     mount_devise_token_auth_for 'User', at: 'auth'
   end
-  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
