28632b3fb0120b4b6437f5bbb6efe615d3ecf15a
yodev <yodev2177@gmail.com>
2021-01-16 18:40:37 +0900

    Add TaskNew

---
+++ vuejs-http/src/components/TaskNew.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <v-btn @click="toggleTaskModal">New Task</v-btn>
+    <v-dialog v-model="dialogPostFlag" width="800" persistent>
+      <v-card>
+        <v-card-title class="headline blue lighten-3 white--text" primary-title>
+          タスク作成
+        </v-card-title>
+        <v-text-field v-model="title" label="Task" required style="margin: 20px"></v-text-field>
+        <v-flex d-flex>
+          <v-text-field v-model="content" label="content" required style="margin: 20px"></v-text-field>
+        </v-flex>
+        <v-card-actions>
+          <v-btn color="#gray lighten-4" flat @click="toggleTaskModal">
+            Cansel
+          </v-btn>
+          <v-spacer></v-spacer>
+          <v-btn @click="create_task">Add Task</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  data(){
+    return {
+      title: '',
+      content: '',
+      dialogPostFlag: false,
+    }
+  },
+  computed: {
+    uidToken(){
+      return this.$store.getters.uidToken
+    },
+    accessToken(){
+      return this.$store.getters.accessToken
+    },
+    clientToken(){
+      return this.$store.getters.clientToken
+    }
+  },
+  methods: {
+    toggleTaskModal(){
+      this.dialogPostFlag = !this.dialogPostFlag
+    },
+
+    create_task() {
+      axios
+        .post("http://localhost:3000/v1/tasks", {
+          uid: this.uidToken,
+          "access-token": this.accessToken,
+          client: this.clientToken,
+          title: this.title,
+          content: this.content,
+        })
+        .then((response) => {
+          console.log(response);
+          this.all_tasks();
+        });
+
+          this.toggleTaskModal();
+    },
+  }
+}
+</script>
--- vuejs-http/src/views/Tasks.vue
+++ vuejs-http/src/views/Tasks.vue
@@ -1,40 +1,16 @@
 <template>
   <div id="app">
     <TaskIndex></TaskIndex>
-      <v-btn @click="toggleTaskModal">New Task</v-btn>
-      <v-dialog v-model="dialogPostFlag" width="800" persistent>
-        <v-card>
-          <v-card-title class="headline blue lighten-3 white--text" primary-title>
-            タスク作成
-          </v-card-title>
-          <v-text-field v-model="title" label="Task" required style="margin: 20px"></v-text-field>
-          <v-flex d-flex>
-            <v-text-field v-model="content" label="content" required style="margin: 20px"></v-text-field>
-          </v-flex>
-          <v-card-actions>
-            <v-btn color="#gray lighten-4" flat @click="toggleTaskModal">
-              Cansel
-            </v-btn>
-            <v-spacer></v-spacer>
-            <v-btn @click="create_task">Add Task</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
+    <TaskNew></TaskNew>
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import TaskIndex from '../components/TaskIndex'
+import TaskNew from '../components/TaskNew'
+
 export default {
-  data() {
-    return {
-      name: "",
-      title: "",
-      content: "",
-      dialogPostFlag: false,
-    };
-  },
   computed: {
     uidToken(){
       return this.$store.getters.uidToken
@@ -49,16 +25,12 @@ export default {
   name: 'Tasks',
   components: {
     TaskIndex,
+    TaskNew
   },
   created(){
-    console.log('fire')
     this.$store.dispatch('reload')
-    // this.all_tasks()
   },
   methods: {
-    toggleTaskModal(){
-      this.dialogPostFlag = !this.dialogPostFlag
-    },
     find_task(task_id) {
       axios
         .get(`http://localhost:3000/v1/tasks/${task_id}`, {
@@ -72,22 +44,6 @@ export default {
           console.log(response);
           this.task = response.data;
         });
-    },
-    create_task() {
-      axios
-        .post("http://localhost:3000/v1/tasks", {
-          uid: this.uidToken,
-          "access-token": this.accessToken,
-          client: this.clientToken,
-          title: this.title,
-          content: this.content,
-        })
-        .then((response) => {
-          console.log(response);
-          this.all_tasks();
-        });
-
-          this.toggleTaskModal();
     },
     delete_task(task_id) {
       axios
