c6bf8d3bb88b3d3d15773767125f555b499a4db7
yodev <yodev2177@gmail.com>
2021-01-16 10:30:58 +0900

    Add vuetify

--- vuejs-http/src/main.js
+++ vuejs-http/src/main.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router';
 import store from './store'
+import Vuetify from 'vuetify';
+
 Vue.config.productionTip = false
+const vuetify = new Vuetify
+Vue.use(Vuetify)
 store.dispatch('autoLogin');
 new Vue({
+  vuetify,
   router,
   store,
   render: h => h(App),
--- vuejs-http/src/views/Register.vue
+++ vuejs-http/src/views/Register.vue
@@ -6,6 +6,11 @@
     <label for="password">Password</label>
     <input id="password" type="password" v-model="password" />
     <button @click="register">SignUp</button>
+          <v-toolbar color="red lighten-3" dark> <!-- 説明 3.4 -->
+        <v-toolbar-title>Snippet App</v-toolbar-title> <!-- 説明 3.5 -->
+        <v-spacer></v-spacer> <!-- 説明 3.6 -->
+        <v-btn flat>New Snippet</v-btn> <!-- 説明 3.7 -->
+      </v-toolbar>
   </div>
 </template>
 
